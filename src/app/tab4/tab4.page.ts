f3ffe4a749d2a0d709409e576154bb93db782ab2
Jorge <ing.jorge.espinoza@outlook.com>
2024-10-29 21:52:14 -0300

    logout, arregle tambien el login

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FirebaseService } from '../service/firebase.service';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+// import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-tab4',
@@ -7,9 +11,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Tab4Page implements OnInit {
 
-  constructor() { }
+  userEmail: string = ''; // Inicialización de la propiedad
+
+  constructor(
+    private firebaseService: FirebaseService,
+    private router: Router,
+    private alertController: AlertController,
+    // private afAuth: AngularFireAuth
+  ) { }
 
   ngOnInit() {
   }
 
+  logout() {
+    this.presentLogoutConfirmation();
+  }
+
+  async presentLogoutConfirmation() {
+    const alert = await this.alertController.create({
+      header: 'Confirmar cierre de sesión',
+      message: '¿Estás seguro de que deseas cerrar sesión?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cierre de sesión cancelado');
+          }
+        }, {
+          text: 'Cerrar sesión',
+          handler: async () => {
+            try {
+              await this.firebaseService.logout();
+              this.router.navigate(['/login']);
+            } catch (error) {
+              console.error('Error al cerrar sesión:', error);
+            }
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
 }
