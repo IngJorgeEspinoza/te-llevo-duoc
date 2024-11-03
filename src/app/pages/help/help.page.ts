2a0dbb1c5d545f8a6b02b05a929426e99bd6b7bc
Jorge <ing.jorge.espinoza@outlook.com>
2024-11-03 01:52:03 -0300

    cambios en el help

--- src/app/pages/help/help.page.ts
+++ src/app/pages/help/help.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-help',
@@ -7,12 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HelpPage implements OnInit {
 
-  supportEmail: string = 'soporte@tellevo.duoc.cl';
-  contactEmail: string = 'soporte@tellevo.duoc.cl';
+  supportEmail: string = 'jor.espinozaa@duocuc.cl';
+  contactEmail: string = 'jor.espinozaa@duocuc.cl';
+  contactPhone: string = '+56 9 9137 9473'; // Número de contacto
+  whatsappNumber: string = '56991379473'; // Número de WhatsApp sin símbolos ni espacios
+  whatsappMessage: string = 'Hola, necesito ayuda con la aplicación Te Llevo DUOC.';
 
-  constructor() { }
+  constructor(private platform: Platform) { }
 
   ngOnInit() {
+  }
+
+  openWhatsApp() {
+    const message = encodeURIComponent(this.whatsappMessage);
+    const url = `https://wa.me/${this.whatsappNumber}?text=${message}`;
+
+    if (this.platform.is('capacitor') || this.platform.is('cordova')) {
+      // Para dispositivos móviles
+      window.open(url, '_system');
+    } else {
+      // Para navegadores de escritorio
+      window.open(url, '_blank');
+    }
   }
 
 }
